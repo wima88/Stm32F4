5e02dc2a97b84dbfd862022ed19238fe01474d8f
Wimansha Wijekoon <wimanshahb@gmail.com>
2020-03-29 18:48:50 -0400

    change the timming
    change the timmer setting for 1Hz

--- F4-randCAN/Src/main.c
+++ F4-randCAN/Src/main.c
@@ -254,7 +254,7 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 35999;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 200;   // [val]/2 ~ time in ms
+  htim4.Init.Period = 2000;   // [val]/2 ~ time in ms
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
